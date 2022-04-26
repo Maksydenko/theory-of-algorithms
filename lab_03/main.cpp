aa3c017c90a96ba82cc59f9fb3e87c385cb5a893
Maksym Davydenko <maksydenko@gmail.com>
2022-04-27 00:32:07 +0300

    Update main.cpp

--- lab_03/main.cpp
+++ lab_03/main.cpp
@@ -5,6 +5,7 @@ int num_comparisons = 0;
 int lineSearch(int arr[], int size, int key) {
 	for (int i = 0; i < size; i++) {
 		num_comparisons++;
+
 		if (arr[i] == key)
 			return i;
 	}
@@ -12,9 +13,11 @@ int lineSearch(int arr[], int size, int key) {
 }
 int biSearch(int arr[], int L, int R, int key) {
 	int mid;
+
 	do {
 		mid = L + (R - L) / 2;
 		num_comparisons++;
+
 		if (key < arr[mid])
 			R = --mid;
 		else {
@@ -40,14 +43,17 @@ int main() {
 	int B[number];
 	int lnum = 0;
 	int bnum = 0;
+
 	for (int i = 0; i < number; i++) {
 		B[i] = rand() % 1000;
 	}
 	mySort(B, number);
 	cout << "Array B:" << endl;
+
 	for (int b : B)
 		cout << b << " ";
 	cout << endl << "Elements 0 to 999 in array B (linear search):" << endl;
+
 	for (int b : B) {
 		if (0 <= b <= 999)
 			if (lineSearch(B, number, b) >= 0)
@@ -57,12 +63,14 @@ int main() {
 	cout << endl << "Number of elements from 0 to 999 in array B (linear search): " << lnum;
 	cout << endl << "Number of comparisons(linear search): " << num_comparisons << endl;
 	cout << endl << "Elements 0 to 999 in array B (binary search):" << endl;
+
 	for (int b : B) {
 		if (0 <= b <= 999)
 			if (biSearch(B, 0, number - 1, b) >= 0)
 				bnum++;
 		cout << b << " ";
 	}
+
 	cout << endl << "Number of elements from 0 to 999 in array B (binary search): " << bnum;
 	cout << endl << "Number of comparisons(linear search): " << num_comparisons;
 }
