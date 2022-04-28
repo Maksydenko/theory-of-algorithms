7c19abbc6fd282dbe02368e9287ccc2299b17097
Maksym Davydenko <maksydenko@gmail.com>
2022-04-29 00:12:12 +0300

    Create main.cpp

---
+++ control_02/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+        cout << i * i << endl;
+
+    const int size = 10;
+    int arr[size];
+    int num_negative = 0;
+
+    for (int i = 0; i < size; i++)
+        arr[i] = rand() % 200 - 100;
+   
+    for (int a : arr)
+    {
+        cout << a << " ";
+        if (a < 0)
+            num_negative++;
+    }
+    cout << endl << "The number of negative numbers in the array: " << num_negative;
+}
