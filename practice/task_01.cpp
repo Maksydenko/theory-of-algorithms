e9876eb9b269e04f02c68ec80f0e730b6d975443
Maksydenko <maksydenko@gmail.com>
2022-05-26 16:01:03 +0300

    Update task_01.cpp

--- practice/task_01.cpp
+++ practice/task_01.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct Node
-{
+struct Node {
 	string product;
 	double price;
 	Node* p;
@@ -12,8 +11,7 @@ void push(Node** top, string product);
 void push(Node** top, double price);
 void pop(Node** top);
 
-void push(Node** top, string product, double price)
-{
+void push(Node** top, string product, double price) {
 	Node* pv = new Node;
 	pv->product = product;
 	pv->price = price;
@@ -21,23 +19,20 @@ void push(Node** top, string product, double price)
 	*top = pv;
 }
 
-void pop(Node** top)
-{
+void pop(Node** top) {
 	Node* pv = *top;
 	*top = (*top)->p;
 	delete pv;
 }
 
-int main()
-{
+int main() {
 	Node* top;
 	top = NULL;
 	string products[10]{ "oranges", "lemons", "melons", "apples", "tomatoes", "carrots", "strawberries", "cucumbers", "cabbage", "kiwi" };
 	double prices[10]{ 1.5, 1, 1, 0.5, 3, 1, 7, 2, 1, 3 };
 	cout << "The store orders products for 100 kilograms:\n";
 
-	for (int i = 0; i < 10; i++)
-	{
+	for (int i = 0; i < 10; i++) 	{
 		cout << "Put in the truck " << products[i] << " which costs " << prices[i] << "$/kg\n";
 		push(&top, products[i], prices[i]);
 	}
@@ -45,8 +40,7 @@ int main()
 	cout << "\nGrocery being taken out of the truck:\n";
 	double sum;
 
-	while (top)
-	{
+	while (top) {
 		double paid = top->price * 100;
 		sum = sum + paid;
 		cout << "They take from it " << top->product << ". The store pays for it " << paid << "$\n";
